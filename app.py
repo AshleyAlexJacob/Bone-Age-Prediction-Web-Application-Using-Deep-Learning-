6b72d0f01d387fce78f5f3589533748679d68144
AshleyAlexJacob <ashleyalexjacob@Ashleys-MacBook-Pro.local>
2022-09-18 14:13:05 +0500

    animal model working

--- app.py
+++ app.py
@@ -1,17 +1,42 @@
 import os
 import uuid
-from flask import Flask, render_template, request, send_file
+from flask import Flask, render_template, request, send_file,session
 import urllib
 from PIL import Image
 from keras.models import load_model
 from keras.utils import load_img, img_to_array
-
+from keras.metrics import mean_absolute_error
+from werkzeug.utils import secure_filename
+import secrets
+# from werkzeug.datastructures import  FileStorage
 
 app = Flask(__name__)
 
 
 base_dir = os.path.dirname(os.path.abspath(__file__))
+
+def mae_in_months(x, y,std_bone_age,mean_bone_age):
+    return mean_absolute_error((std_bone_age*x + mean_bone_age), 
+                               (std_bone_age*y + mean_bone_age))
+
 model = load_model(os.path.join(base_dir,'models/model.hdf5'))
+# model = load_model(os.path.join(base_dir,'models/xception_weak_model.hdf5'),compile=False,custom_objects={"mae_in_months": mae_in_months}); 
+
+UPLOAD_FOLDER = os.path.join('static', 'uploads')
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+app.secret_key = secrets.token_hex(16)
+
+
+def deleteFiles():
+    app.logger.info('Starting')
+    for file_name in os.listdir(UPLOAD_FOLDER):
+        file = UPLOAD_FOLDER +'/' + file_name
+        if os.path.isfile(file):
+            app.logger.info('Deleting file:', file)
+            os.remove(file)
 
 
 # Adding allowed extensions of images only
@@ -57,22 +82,29 @@ def runModel(filename , model):
 @app.route("/",methods=['GET'])
 @app.route("/index",methods=['GET'])
 def hello_world():
+    deleteFiles()
     return render_template('index.html')
 
-@app.route("/predict/",methods=['GET','POST'])
+
+@app.route("/predict",methods=['GET','POST'])
 
 def predict():
     error = ''
-    target_img = os.path.join(os.getcwd() , 'static/images')
+    
     if request.method == 'POST':
         if (request.files):
+            file = request.files['file']
+            # file_name = secure_filename(file.filename)
+            # file.save(os.path.join(app.config['UPLOAD_FOLDER'], file_name))
+            # session['upload_image'] = os.path.join(app.config['UPLOAD_FOLDER'], file_name)
+            # return render_template('image.html',image = os.path.join(app.config['UPLOAD_FOLDER'],file_name))
+            
             if file and allowed_file(file.filename):
-                file = request.files['file']
-                file.save(os.path.join(target_img , file.filename))
-                img_path = os.path.join(target_img , file.filename)
+                file_name = secure_filename(file.filename)
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], file_name))
                 img = file.filename
-                class_result , prob_result = predict(img_path , model)
-                predictions = {
+                class_result , prob_result = runModel((os.path.join(app.config['UPLOAD_FOLDER'], img)) , model)
+                results = {
                         "class1":class_result[0],
                             "class2":class_result[1],
                             "class3":class_result[2],
@@ -80,15 +112,17 @@ def predict():
                             "prob2": prob_result[1],
                             "prob3": prob_result[2],
                     }
+                # return predictions
+                return render_template('success.html',predictions=results,image= os.path.join(app.config['UPLOAD_FOLDER'],file_name))
             else:
                 error = "Please upload images of jpg , jpeg and png extension only"
             
-            if(len(error) == 0):
-                return  render_template('predict.html' , img  = img , predictions = predictions)
-            else:
-                return render_template('index.html' , error = error)
+            
+        else:
+            return  {'error': 'Please add Files'}
 
-
+    else:
+        return {'error':'Requires post method'}
             
 
 
