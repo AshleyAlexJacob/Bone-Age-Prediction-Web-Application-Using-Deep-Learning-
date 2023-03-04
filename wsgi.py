69d77e3c9ecde1241f747b62f855cf4bd74cccc5
AshleyAlexJacob <alexjacob260@gmail.com>
2023-03-04 14:51:19 +0500

    📦 NEW: added vercel.json

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,8 @@
 from app.app import app
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    # app.run()
+    # app.run(debug=True)
+    app.run(port=8080)
     
     # app.run(host="0.0.0.0", port=8080)
 
